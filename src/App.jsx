81d60daeb3000f8834c0179ee42bbcbc6ed442d3
balombialog <lopezalejostring@gmail.com>
2024-12-11 00:42:46 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
 import NavBar from './components/NavBar'
 import Header from './components/Header'
 import Section from './components/Section'
+import Modal from './components/Modal'
+import StateAcount from './components/StateAcount';
 
-export default function App() {
+const App = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [isLoged, setIsLoged] = useState(false);
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const openModal = () => setShowModal(true);
+  const closeModal = () => setShowModal(false);
+  
+  const handleSubmit = (user) => {
+    setIsLoged(true); // Cambia el estado a logueado
+    setCurrentUser(user); 
+    closeModal(); // Cierra el modal 
+  };
+
   return (
-    <div className='pt-52'><NavBar/>
-    <Header/>
-    <Section/></div>
+    <div className='pt-52'>
+    {isLoged 
+          ? <div><NavBar/>
+            <StateAcount user ={currentUser}/>
+            <Header/>
+            <Section/>
+
+          </div>
+          :<div>
+          <NavBar openModal={openModal} />
+          <Modal showModal={showModal} closeModal={closeModal} onLogin={handleSubmit} />
+          <Header/>
+          <Section/></div>}
+    </div>
   )
 }
