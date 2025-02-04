1608e59d1db1bf645269e0a08f831f998b809fea
balombialog <lopezalejostring@gmail.com>
2025-02-04 13:35:42 -0500

    Update mockUser.js

--- src/data/mockUser.js
+++ src/data/mockUser.js
@@ -114,7 +114,7 @@ const mockUsers = [
   {
     id: 13,
     name: "Juancarlos",
-    email: "Juaneuropartes@gmail.com",
+    email: "Juaneuroparts@gmail.com",
     password: "juanel789",
     accountType: "Cuenta de Ahorro",
     accountNumber: "232-879435-13",
