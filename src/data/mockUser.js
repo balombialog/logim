de83112ab8b02ee35ade2726fb3ff10684448fc7
balombialog <lopezalejostring@gmail.com>
2025-05-02 18:01:47 -0500

    Update mockUser.js

--- src/data/mockUser.js
+++ src/data/mockUser.js
@@ -7,48 +7,67 @@ const mockUsers = [
     accountType: "Cuenta de Ahorro",
     accountNumber: "678865676567",
     balance: 3520000000.00,
-    messages: ["tu cuenta fiduciaria terminda 6567 se realizo un abono 35.200.000.00", "estado de cuenta retenido", "impuesto del gobierno 4x1000"]
+    messages: [
+      "Tu cuenta fiduciaria terminada en 6567 se realizó un abono de 35,200,000.00",
+      "Estado de cuenta retenido",
+      "Impuesto del gobierno 4x1000"
+    ]
   },
   {
     id: 2,
-    name: "christia vele",
+    name: "Christia Vele",
     email: "chrodriguez015@gmail.com",
     password: "2345",
-    accountType: "Cuenta ahorros",
+    accountType: "Cuenta de Ahorro",
     accountNumber: "778865676567",
     balance: 12800000.00,
-    messages: ["tu cuenta fiduciaria terminda 6567 se realizo un abono 12.800.000.00", "estado de cuenta retenido", "impuesto del gobierno 4x1000"]
+    messages: [
+      "Tu cuenta fiduciaria terminada en 6567 se realizó un abono de 12,800,000.00",
+      "Estado de cuenta retenido",
+      "Impuesto del gobierno 4x1000"
+    ]
   },
   {
     id: 4,
-    name: "zulma denise cabrera",
+    name: "Zulma Denise Cabrera",
     email: "kikiyponky@gmail.com",
     password: "2456",
     accountType: "Cuenta Corriente",
     accountNumber: "778865676567",
-    balance: 00,
-    messages: ["se realizo una abono a su cuenta fiduciaria terminda 6567 por un valor 00", "esrado de cuenta rembolsado","MS"]
-    
+    balance: 0.00,
+    messages: [
+      "Se realizó un abono de 0.00 a su cuenta fiduciaria terminada en 6567",
+      "Estado de cuenta reembolsado",
+      "MS"
+    ]
   },
   {
     id: 5,
-    name: "alicia nava",
+    name: "Alicia Nava",
     email: "lixinava24@hotmail.com",
     password: "2356",
     accountType: "Cuenta de Ahorro",
     accountNumber: "778-8656765-67",
     balance: 257300000.00,
-    messages: ["se realizo una abono a su cuenta fiduciaria terminda 6567 por un valo 257,300,000", "estado de cuenta retenida", "impuesto 19%"]
+    messages: [
+      "Se realizó un abono de 257,300,000.00 a su cuenta fiduciaria terminada en 6567",
+      "Estado de cuenta retenida",
+      "Impuesto 19%"
+    ]
   },
   {
     id: 6,
-    name: "pedro jose delgado",
+    name: "Pedro Jose Delgado",
     email: "pedroj.delgado1989@gmail.com",
     password: "4321",
-    accountType: "Cuenta ahorros",
+    accountType: "Cuenta de Ahorro",
     accountNumber: "078-865656765-67",
     balance: 12100000.00,
-    messages: ["se realizo una abono a su cuenta fiduciaria terminda 6567 por un valo 12,100,000,000", "estado de cuenta retenida", "impuesto del gobierno 4x1000"]
+    messages: [
+      "Se realizó un abono de 12,100,000,000.00 a su cuenta fiduciaria terminada en 6567",
+      "Estado de cuenta retenido",
+      "Impuesto del gobierno 4x1000"
+    ]
   },
   {
     id: 7,
@@ -58,38 +77,53 @@ const mockUsers = [
     accountType: "Cuenta de Ahorro",
     accountNumber: "232-879435-07",
     balance: 769505000.00,
-    messages: ["realizo una abono a su cuenta fiduciaria terminda 3507 por un valo 769,505,000,00 ", "estado de cuenta retenida", "TRM cambio de divisa "]
-    
+    messages: [
+      "Realizó un abono de 769,505,000.00 a su cuenta fiduciaria terminada en 3507",
+      "Estado de cuenta retenido",
+      "TRM cambio de divisa"
+    ]
   },
   {
     id: 8,
     name: "Victos M",
     email: "villatorov339@gmail.com",
     password: "2345",
-    accountType: "Cuenta ahorros",
+    accountType: "Cuenta de Ahorro",
     accountNumber: "232-879435-08",
-    balance: 14721984485,
-    messages: ["realizo una abono a su cuenta fiduciaria terminda 3508 por un valo 14,721,984,485", "estado de cuenta retenida", "impuesto del gobierno 4x1000"]
+    balance: 14721984485.00,
+    messages: [
+      "Realizó un abono de 14,721,984,485.00 a su cuenta fiduciaria terminada en 3508",
+      "Estado de cuenta retenido",
+      "Impuesto del gobierno 4x1000"
+    ]
   },
   {
     id: 9,
-    name: "Francisca raudal",
+    name: "Francisca Raudal",
     email: "Francisraud2015@gmail.com",
     password: "2121",
-    accountType: "Cuenta ahorro",
+    accountType: "Cuenta de Ahorro",
     accountNumber: "778-8656765-67",
-    balance: 2057500000,
-    messages: ["se realiza una traferencia por un valor 701,813,250 al banco TD BANK ", "# 4444669977 RUTA 067014822 ", " transferencia internacional"] 
+    balance: 2057500000.00,
+    messages: [
+      "Se realizó una transferencia por un valor de 701,813,250.00 al banco TD BANK",
+      "# 4444669977 RUTA 067014822",
+      "Transferencia internacional"
+    ]
   },
   {
     id: 10,
-    name: "Rafael fuentes",
+    name: "Rafael Fuentes",
     email: "cundy7175@gmail.com",
     password: "6789",
-    accountType: "Cuenta ahorros",
+    accountType: "Cuenta de Ahorro",
     accountNumber: "678865676567",
     balance: 13300000.00,
-    messages: ["tu cuenta fiduciaria terminda 6567 se realizo un abono 13.300.000.00", "estado de cuenta retenido", "impuesto del gobierno 4x1000"]
+    messages: [
+      "Tu cuenta fiduciaria terminada en 6567 se realizó un abono de 13,300,000.00",
+      "Estado de cuenta retenido",
+      "Impuesto del gobierno 4x1000"
+    ]
   },
   {
     id: 11,
@@ -99,7 +133,11 @@ const mockUsers = [
     accountType: "Cuenta de Ahorro",
     accountNumber: "232-879435-11",
     balance: 400.00,
-    messages: ["tu cuenta fiduciaria terminda 6567 se realizo un abono 35.200.000.00", "estado de cuenta retenido", "impuesto del gobierno 4x1000"]
+    messages: [
+      "Tu cuenta fiduciaria terminada en 6567 se realizó un abono de 35,200,000.00",
+      "Estado de cuenta retenido",
+      "Impuesto del gobierno 4x1000"
+    ]
   },
   {
     id: 12,
@@ -109,27 +147,39 @@ const mockUsers = [
     accountType: "Cuenta Corriente",
     accountNumber: "232-879435-12",
     balance: 3100.00,
-    messages: ["tu cuenta fiduciaria terminda 6567 se realizo un abono 35.200.000.00", "estado de cuenta retenido", "impuesto del gobierno 4x1000"]
+    messages: [
+      "Tu cuenta fiduciaria terminada en 6567 se realizó un abono de 35,200,000.00",
+      "Estado de cuenta retenido",
+      "Impuesto del gobierno 4x1000"
+    ]
   },
   {
     id: 13,
-    name: "Benilda romero",
+    name: "Benilda Romero",
     email: "romerobenilda67@gmail.com",
     password: "2356",
     accountType: "Cuenta de Ahorro",
     accountNumber: "232-879435-13",
     balance: 187211250.00,
-    messages: ["tu cuenta fiduciaria terminda 3513 se realizo un abono 187.211.250.00", "estado de cuenta retenido", "impuesto del gobierno 4x1000"] 
+    messages: [
+      "Tu cuenta fiduciaria terminada en 3513 se realizó un abono de 187,211,250.00",
+      "Estado de cuenta retenido",
+      "Impuesto del gobierno 4x1000"
+    ]
   },
   {
     id: 14,
-    name: "Juan carlos ",
+    name: "Juan Carlos",
     email: "juaneuroparts@gmail.com",
     password: "5473",
     accountType: "Cuenta Corriente",
     accountNumber: "232-879435-14",
     balance: 77687085.00,
-    messages: ["tu cuenta fiduciaria terminda 3514 se realizo un abono 77.687.085.00", "estado de cuenta retenido", "impuesto del gobierno 4x1000"] // Mensajes vacíos
+    messages: [
+      "Tu cuenta fiduciaria terminada en 3514 se realizó un abono de 77,687,085.00",
+      "Estado de cuenta retenido",
+      "Impuesto del gobierno 4x1000"
+    ]
   },
   {
     id: 15,
@@ -139,37 +189,45 @@ const mockUsers = [
     accountType: "Cuenta de Ahorro",
     accountNumber: "778-8656765-67",
     balance: 3857812500.00,
-    messages: [" tu cuenta fiduciaria terminda 6567 se realizo un abono 3.857.812.500.00", "estado de cuenta retenido", "impuesto del gobierno 4x1000"] // Mensajes vacíos
+    messages: [
+      "Tu cuenta fiduciaria terminada en 6567 se realizó un abono de 3,857,812,500.00",
+      "Estado de cuenta retenido",
+      "Impuesto del gobierno 4x1000"
+    ]
   },
   {
     id: 16,
     name: "Josefa Ch",
     email: "josefamatzar28@gmail.com",
     password: "4987",
-    accountType: "Cuenta ahorros",
+    accountType: "Cuenta de Ahorro",
     accountNumber: "232-879435-16",
     balance: 11360000000.00,
-    messages: [" tu cuenta fiduciaria terminda 3516 se realizo un abono 11.360.000.000.00", "estado de cuenta retenido", "impuesto del gobierno 4x1000"] // Mensajes vacíos
+    messages: [
+      "Tu cuenta fiduciaria terminada en 3516 se realizó un abono de 11,360,000,000.00",
+      "Estado de cuenta retenido",
+      "Impuesto del gobierno 4x1000"
+    ]
   },
   {
     id: 17,
-    name: "Julio Cesar Lucero sucheite",
+    name: "Julio Cesar Lucero Sucheite",
     email: "Davidbro2000@icloud.com",
     password: "2456",
     accountType: "Cuenta de Ahorro",
     accountNumber: "232-879435-17",
-    balance: 00,
-    messages: [] // Mensajes vacíos
+    balance: 0.00,
+    messages: []
   },
   {
     id: 18,
-    name: "Froilan galvez",
+    name: "Froilan Galvez",
     email: "F82235550@gmail.com",
     password: "l789",
     accountType: "Cuenta Corriente",
     accountNumber: "232-879435-18",
-    balance: 00,
-    messages: [] // Mensajes vacíos
+    balance: 0.00,
+    messages: []
   },
   {
     id: 19,
@@ -179,7 +237,7 @@ const mockUsers = [
     accountType: "Cuenta de Ahorro",
     accountNumber: "232-879435-19",
     balance: 600.00,
-    messages: [] // Mensajes vacíos
+    messages: []
   },
   {
     id: 20,
@@ -189,7 +247,7 @@ const mockUsers = [
     accountType: "Cuenta Corriente",
     accountNumber: "232-879435-20",
     balance: 1350.00,
-    messages: [] // Mensajes vacíos
+    messages: []
   }
 ];
 
