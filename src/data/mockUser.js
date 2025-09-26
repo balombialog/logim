1551c0cca75d95b0c2295e2ca43d7ade05e3d95a
balombialog <lopezalejostring@gmail.com>
2025-09-26 13:48:58 -0500

    Update mockUser.js

--- src/data/mockUser.js
+++ src/data/mockUser.js
@@ -209,7 +209,7 @@ const mockUsers = [
     id: 17,
     name: "MARCELINO GONZALES ",
     email: "gloriav2003@icloud.com",
-    password: "4887",
+    password: "4987",
     accountType: "Cuenta de Ahorro",
     accountNumber: "67-886-567-656-7",
     balance: 16200000000.00,
